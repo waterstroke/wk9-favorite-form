2c3c731cc56f3f54798766ce35763d5071a2a299
tory bryant <waterstroke@aol.com>
2017-06-13 10:18:22 -0700

    it will work

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 $(document).ready(function() {
 
-  $("form#user-faves").submit(function(event) {
-    var favDog = $("input#favDog").val();
-    var favFruit = $("input#favFruit").val();
-    var favColor = $("input#favColor").val();
-    var favPlace = $("input#favPlace").val();
+  $("#blanks form").submit(function(event) {
+    // var favDog = $("input#favDog").val();
+    // var favFruit = $("input#favFruit").val();
+    // var favColor = $("input#favColor").val();
+    // var favPlace = $("input#favPlace").val();
+    var blanks = ["favDog","favFruit","favColor","favPlace"];
 
-    $(".fillInDog").text(favDog);
-    $(".fillInFruit").text(favFruit);
-    $(".fillInColor").text(favColor);
-    $(".fillInPlace").text(favPlace);
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      console.log(blank);
+      $("." + blank).text(userInput)
+     });
+    //
+
+    // $(".fillInDog").text(favDog);
+    // $(".fillInFruit").text(favFruit);
+    // $(".fillInColor").text(favColor);
+    // $(".fillInPlace").text(favPlace);
 
     $("#story").show();
+
     event.preventDefault();
   });
 });
